59933923568a2acb26de727cf86360705d4e6a02
MartijnVDV <m.l.a.vandervoort@student.tudelft.nl>
2024-11-26 20:07:26 +0100

    update

--- Moment_of_Inertia2.py
+++ Moment_of_Inertia2.py
@@ -1,7 +1,12 @@
 #Moment of inertia and torsional stiffness diagrams
-from Parameters import c_Root, taper_Ratio, b
+from Parameters import c_Root, taper_Ratio
 
 #Chord length as function of spanwise position
-def chord_Length(c_Root, taper_Ratio, b):
-    for i in range 10000:
-        chord_Local = c_Root - c_Root*(1-taper_Ratio)*i/100000
+def chord_Length(c_Root, taper_Ratio):
+    list_Chords = []
+    for i in range(1,1000000):
+        chord_Local = c_Root - c_Root*(1-taper_Ratio)*(i/1000000)
+        list_Chords.append(chord_Local)
+    return list_Chords
+
+print(chord_Length)
