9125dcd186da0800180377263e19bc9f6fb4205b
Odin Tijssen <otijssen@tudelft.nl>
2024-12-07 14:57:12 +0100

    new file + update

---
+++ DeflectionPlot.py
@@ -0,0 +1,15 @@
+from DeflectionFinal import divider, int_1, int_2
+from ShearDiagram import zAxis
+from Parameters import b
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+z = np.arange(0, b/2, 0.25)
+value = np.ones_like(z)
+
+for i in range(z.size):
+    value[i] = int_2(z[i])
+
+plt.plot(z, value)
+plt.show()
