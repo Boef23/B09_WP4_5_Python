9a386fd92e8743ffa76af0bd719a1e8bc6e93812
Boef23 <basvanstrien@gmail.com>
2024-12-12 17:10:48 +0100

    commuit

--- Skin Buckling.py
+++ Skin Buckling.py
@@ -1,7 +1,7 @@
 from Parameters import *
 def enclosedarea(z):
     chord = c_Root - c_Root*(1-taper_Ratio) * (z/(0.5 * b))
-    h_Fs = 0.0732 * chord #height of front spar
+    h_Fs = 0.1092 * chord #height of front spar
     h_Bs = 0.0732 * chord #height of back spar
     l_wb = 0.5 * chord #Length 
     A_m = ((h_Bs+h_Fs)/2)*l_wb
@@ -14,4 +14,3 @@ def Ymaxfinder(z, Y_cen):
     chord = c_Root - c_Root*(1-taper_Ratio) * (z/(0.5 * b))
     y_ref = Y_cen
     t_max_airfoil = 0.12*chord
-    
