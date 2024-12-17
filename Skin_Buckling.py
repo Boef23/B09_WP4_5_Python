e85c01ed6fad82ac192ad907f5536aaad8557ffe
Boef23 <basvanstrien@gmail.com>
2024-12-17 14:24:51 +0100

    changes

--- Skin_Buckling.py
+++ Skin_Buckling.py
@@ -15,7 +15,7 @@ def Ymaxfinder(z, Y_cen):
     t_max_airfoil = 0.12*chord
     h_Fs = 0.1092 * chord #height of front spar
     yskin_fs_atTmax = (t_max_airfoil - h_Fs)/2
-    ymaxtop = Y_cen + yskin_fs_atTmax
+    ymaxtop = -(Y_cen + yskin_fs_atTmax)
     ymaxbottom = abs(h_Fs - Y_cen + yskin_fs_atTmax)
     if ymaxtop >= ymaxbottom:
         return ymaxtop
