4dd843ab828e780f4d654fa0b4745f519e4d2cc9
MartijnVDV <m.l.a.vandervoort@student.tudelft.nl>
2024-12-19 14:52:08 +0100

    update

--- Moment_of_inertia_3.py
+++ Moment_of_inertia_3.py
@@ -58,3 +58,28 @@ def calculate_Centroid_wingbox(z, b=b, t_Fs = t_Fs, t_Bs = t_Bs, t_Bottom = t_Bo
 def calculate_paramters_per_Bay():
      #Get the bay values
      
+def Ixx_Fs():
+    Ixx_Fs = (t_Fs * h_Bs ** 3)/12 + t_Fs * h_Bs * (0.5 * h_Bs - y_centroid) ** 2
+    #From geometry function, h_Bs is used instead of h_Fs due to assumption
+    return Ixx_Fs
+
+def Ixx_Top():
+    Ixx_Top = (l_Top * t_Top ** 3)/12 + l_Top * t_Top * y_centroid ** 2
+    #From geometry function
+    return Ixx_Top
+
+def Ixx_Bs():
+    Ixx_Bs = (t_Bs * h_Bs ** 3)/12 + t_Bs * h_Bs * (0.5 * h_Bs - y_centroid) ** 2
+    #From geometry function
+    return Ixx_Bs
+
+def Ixx_Bottom():
+    Ixx_Bottom = (l_Bottom * t_Bottom ** 3)/12 + l_Bottom * t_Bottom * (h_Bs - y_centroid) ** 2
+    #From geometry function
+    return Ixx_Bottom
+
+def Ixx_Stringers():
+    Ixx_Stringers = (n_Str_Top(i) + n_Str_Bottom(i)) * Ixx_Local_Stringer + n_Str_Top(i) * ((a_Str * t_Str_a + b_Str * t_Str_b) * (y_centroid - y_Centroid_Stringer) ** 2) + n_Str_Bottom(i) * ((a_Str * t_Str_a + b_Str * t_Str_b) * (h_Bs - y_Centroid_Stringer - y_centroid) ** 2))
+    #From list of number of stringers per side and i is increment value to later put this in a loop per bay.
+    #Ixx_Local_Stringer is from local inertia function
+    return Ixx_Stringers
