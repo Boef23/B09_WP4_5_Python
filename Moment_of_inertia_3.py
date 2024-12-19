8922ac42e87e342a6f25eb4eb8757154aec8f004
BjornVerhoef <b.e.g.verhoef@student.tudelft.nl>
2024-12-19 14:20:21 +0100

    changes

--- Moment_of_inertia_3.py
+++ Moment_of_inertia_3.py
@@ -1,6 +1,5 @@
 """I_xx, I_yy calculation"""
 from Parameters import * 
-from 
 
 #Temporary parameters 
 # a = w_Stringer  #[m] width of the stringers 
@@ -13,8 +12,20 @@ from
 # j = 11
 # k = 10
 
+
+
+def calculate_Centroid_Stringer(a_Str = a_str, t_Str_a = t_str_a, b_Str = b_str, t_Str_b = t_str_b):
+     x_Centroid_Stringer = (0.5 * t_Str_b * b_Str ** 2)/(a_Str * t_Str_a + b_Str * t_Str_b)
+     y_Centroid_Stringer = (0.5 * t_Str_a * a_Str ** 2)/(a_Str * t_Str_a + b_Str * t_Str_b)
+     return x_Centroid_Stringer, y_Centroid_Stringer
+
+def calculate_Inertia_Local_Stringer(a_Str = a_str, t_Str_a = t_str_a, b_Str = b_str, t_Str_b = t_str_b, x_Centroid_Stringer = calculate_Centroid_Stringer()[0], y_Centroid_Stringer = calculate_Centroid_Stringer()[1]):
+     Ixx_Local_Stringer = (b_Str * t_Str_b ** 3)/12 + b_Str * t_Str_b * y_Centroid_Stringer ** 2 + (t_Str_a * a_Str ** 3)/12 + a_Str * t_Str_a * (0.5 * a_Str - y_Centroid_Stringer) ** 2
+     Iyy_Local_Stringer = (t_Str_b * b_Str ** 3)/12 + b_Str * t_Str_b * (0.5 * b_Str - x_Centroid_Stringer) ** 2 + (a_Str * t_Str_a ** 3)/12 + a_Str * t_Str_a * x_Centroid_Stringer ** 2
+     return Ixx_Local_Stringer, Iyy_Local_Stringer
+
 #Centroid determination 
-def calculate_Centroid(z, b=b, t_Fs = t_Fs, t_Bs = t_Bs, t_Bottom = t_Bottom, t_Top = t_Top, a = w_Stringer, b  m = n_str_Top, n = n_str_Bottom):
+def calculate_Centroid_wingbox(z, b=b, t_Fs = t_Fs, t_Bs = t_Bs, t_Bottom = t_Bottom, t_Top = t_Top, m = n_str_Top_ztip, n = n_str_Bottom_ztip, t_str_a = t_str_a, t_str_b = t_str_b, a_str = a_str, b_str = b_str):
      #Calculate Coord along Z
      chord = c_Root - c_Root*(1-taper_Ratio) * (z/(0.5 * b))
      c = chord
@@ -25,21 +36,18 @@ def calculate_Centroid(z, b=b, t_Fs = t_Fs, t_Bs = t_Bs, t_Bottom = t_Bottom, t_
      
      #Calculate area of the wing box and area of the stringers
      A_wb = (t_Top + t_Bottom) * l_top + (t_Fs + t_Bs) * h_Bs
-     A_str = (m + n) * (a)
+     A_str = (m + n) * (a_str*t_str_a + b_str*t_str_b)
+
+     #Calculate centroid of stringer
+     y_centroid_str = calculate_Centroid_Stringer()[1]
 
      #Calculate centroid with respect to the middle of the wing bock
-     y_centroid = 
+     y_centroid = ((t_Bottom - t_Top) * (h_Bs*l_top/2) + (m-n)*(h_Bs/2 - y_centroid_str)*A_str)/(A_str+A_wb)
+
+     print(y_centroid)
 
      #Redefine the reference point to top left 
      
-     return x_centroid, y_centroid 
+     return y_centroid 
 
-def calculate_Centroid_Stringer(a_Str, t_Str_a, b_Str, t_Str_b):
-     x_Centroid_Stringer = (0.5 * t_Str_b * b_Str ** 2)/(a_Str * t_Str_a + b_Str * t_Str_b)
-     y_Centroid_Stringer = (0.5 * t_Str_a * a_Str ** 2)/(a_Str * t_Str_a + b_Str * t_Str_b)
-     return x_Centroid_Stringer, y_Centroid_Stringer
-
-def calculate_Inertia_Local_Stringer(a_Str, t_Str_a, b_Str, t_Str_b, x_Centroid_Stringer, y_Centroid_Stringer):
-     Ixx_Local_Stringer = (b_Str * t_Str_b ** 3)/12 + b_Str * t_Str_b * y_Centroid_Stringer ** 2 + (t_Str_a * a_Str ** 3)/12 + a_Str * t_Str_a * (0.5 * a_Str - y_Centroid_Stringer) ** 2
-     Iyy_Local_Stringer = (t_Str_b * b_Str ** 3)/12 + b_Str * t_Str_b * (0.5 * b_Str - x_Centroid_Stringer) ** 2 + (a_Str * t_Str_a ** 3)/12 + a_Str * t_Str_a * x_Centroid_Stringer ** 2
-     return Ixx_Local_Stringer, Iyy_Local_Stringer
+calculate_Centroid_wingbox(1)
