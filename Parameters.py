e277cbf039f40573366d2a76f77b4ef382f06a81
MartijnVDV <m.l.a.vandervoort@student.tudelft.nl>
2025-01-09 16:08:49 +0100

    c

--- Parameters.py
+++ Parameters.py
@@ -3,8 +3,8 @@
 t_skin = 0.007 #(m)
 
 #Stringers
-t_Str_a = 0.019 #Thickness long side (m)
-t_Str_b = 0.020 #Thickness short side
+t_Str_a = 0.01 #Thickness long side (m)
+t_Str_b = 0.01 #Thickness short side
 a_Str = 0.05 #Stringer width in plane of the skin it is attached to (m)
 b_Str = 0.18 #Stringer height out of plane of the skin it is attached to (m)
 
