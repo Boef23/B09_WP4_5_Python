57a2402e7abc06d68d755058f96e4031a2943703
MartijnVDV <m.l.a.vandervoort@student.tudelft.nl>
2025-01-09 15:01:33 +0100

    c

--- Parameters.py
+++ Parameters.py
@@ -6,7 +6,7 @@ t_skin = 0.008 #(m)
 t_Str_a = 0.013 #Thickness long side (m)
 t_Str_b = 0.020 #Thickness short side
 a_Str = 0.05 #Stringer width in plane of the skin it is attached to (m)
-b_Str = 0.19 #Stringer height out of plane of the skin it is attached to (m)
+b_Str = 0.18 #Stringer height out of plane of the skin it is attached to (m)
 
 #Number of stringers plates
 n_Str_Top_ztip = 4
