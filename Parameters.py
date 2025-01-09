d142cc3578a68a5843da50effc7f5970e9215014
MartijnVDV <m.l.a.vandervoort@student.tudelft.nl>
2025-01-09 17:46:49 +0100

    c

--- Parameters.py
+++ Parameters.py
@@ -27,8 +27,8 @@ t_Fs = 0.03 #thickness front spar (m)
 t_Bs = 0.03 #thickness back spar (m)
 
 #Plates
-t_Top = 0.05 #thickness top plate (m)
-t_Bottom = 0.05 #thickness bottom plate (m)
+t_Top = 0.03 #thickness top plate (m)
+t_Bottom = 0.03 #thickness bottom plate (m)
 
 #############################################################################################################
 
