5d3695f2243376a5f018027c2ec0726f9a621342
MartijnVDV <m.l.a.vandervoort@student.tudelft.nl>
2025-01-09 15:35:55 +0100

    c

--- Parameters.py
+++ Parameters.py
@@ -23,12 +23,12 @@ n_Str_Top_incr = [n_Str_Top_ztip, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0]
 n_Str_Bottom_incr = [n_Str_Bottom_ztip, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0]
 
 #Spars
-t_Fs = 0.04 #thickness front spar (m)
-t_Bs = 0.04 #thickness back spar (m)
+t_Fs = 0.03 #thickness front spar (m)
+t_Bs = 0.03 #thickness back spar (m)
 
 #Plates
-t_Top = 0.009 #thickness top plate (m)
-t_Bottom = 0.009 #thickness bottom plate (m)
+t_Top = 0.010 #thickness top plate (m)
+t_Bottom = 0.010 #thickness bottom plate (m)
 
 #############################################################################################################
 
