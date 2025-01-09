51784944ab9ded80ad92bf326451d8bdea63aaab
MartijnVDV <m.l.a.vandervoort@student.tudelft.nl>
2025-01-09 15:22:32 +0100

    c

--- Parameters.py
+++ Parameters.py
@@ -9,18 +9,18 @@ a_Str = 0.05 #Stringer width in plane of the skin it is attached to (m)
 b_Str = 0.05 #Stringer height out of plane of the skin it is attached to (m)
 
 #Number of stringers plates
-n_Str_Top_ztip = 15
-n_Str_Bottom_ztip = 14
+n_Str_Top_ztip = 20
+n_Str_Bottom_ztip = 19
 
 #Increment Stringers Per Bay Top Plate 
 # #Should be even amount
 #First value is basic amount of stringers
-n_Str_Top_incr = [n_Str_Top_ztip, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0]
+n_Str_Top_incr = [n_Str_Top_ztip, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0]
 
 #Increment Stringers Per Bay Top Plate
 #Should be even amount
 #First value is basic amount of stringers
-n_Str_Bottom_incr = [n_Str_Bottom_ztip, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0]
+n_Str_Bottom_incr = [n_Str_Bottom_ztip, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0]
 
 #Spars
 t_Fs = 0.100 #thickness front spar (m)
